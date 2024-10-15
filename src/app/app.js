f4f993ab85c20248b17fcc0d21e7990f53efcaa1
PavelPotapov <pavelpotapovv96@gmail.com>
2024-10-15 19:51:19 +0500

    feat: add api client

--- src/app/app.js
+++ src/app/app.js
@@ -1,10 +1,12 @@
 import "./styles.js";
-import { BASE_URL, API_URL, API_ENDPOINTS } from "#shared/config/constants";
+import { API_URL, API_ENDPOINTS } from "#shared/config/constants";
+import { ApiClient } from "../shared/lib/services/ApiClient.js";
 
 async function initMSW() {
   if (process.env.NODE_ENV === "development") {
     const { getMocks } = await import("#shared/api/browser");
     await getMocks();
+
     console.debug("msw ready");
   } else {
     return Promise.resolve();
@@ -22,10 +24,9 @@ function domReady() {
 }
 
 Promise.all([initMSW(), domReady()]).then(() => {
-  fetch(`${BASE_URL}/${API_URL}/${API_ENDPOINTS.posts.news}`)
-    .then((res) => res.json())
-    .then((res) => console.debug(res));
+  const apiClient = new ApiClient(API_URL);
 
-  const a = "hu";
-  console.debug("Бывает");
+  apiClient
+    .get(API_ENDPOINTS.posts.news, { id: 5 })
+    .then((res) => console.debug(res));
 });
---
+++ src/shared/lib/services/ApiClient.js
@@ -0,0 +1,95 @@
+export class ApiClient {
+  constructor(baseURL) {
+    this.baseURL = baseURL;
+  }
+
+  // Преобразование объекта параметров в строку запроса
+  serializeParams(params) {
+    return new URLSearchParams(params).toString();
+  }
+
+  //TODO: скорее всего это лучше преобразовать в switch case
+  async #handleResponse(response) {
+    const contentType = response.headers.get("Content-Type");
+
+    let responseData;
+    if (contentType.includes("application/json")) {
+      responseData = await response.json();
+    } else if (
+      contentType.includes("text/plain") ||
+      contentType.includes("text/html")
+    ) {
+      responseData = await response.text();
+    } else if (
+      contentType.includes("application/xml") ||
+      contentType.includes("text/xml")
+    ) {
+      responseData = await response.text(); // Можно использовать XMLParser для парсинга
+    } else if (
+      contentType.includes("image/") ||
+      contentType.includes("application/octet-stream")
+    ) {
+      responseData = await response.blob(); // Для изображений или бинарных данных
+    } else {
+      responseData = await response.text();
+    }
+
+    return responseData;
+  }
+
+  async request(
+    endpoint,
+    method = "GET",
+    body = null,
+    headers = {},
+    contentType = "application/json"
+  ) {
+    //Формируем полный URL запроса
+    const url = `${this.baseURL}/${endpoint}`;
+    const options = {
+      method,
+      headers: {
+        "Content-Type": contentType,
+        ...headers,
+      },
+    };
+
+    if (body) {
+      switch (contentType) {
+        case "application/json":
+          options.body = JSON.stringify(body);
+          break;
+        default:
+          options.body = body;
+      }
+    }
+
+    try {
+      const response = await fetch(url, options);
+
+      if (!response.ok) {
+        throw new Error(`Error ${response.status}`);
+      }
+
+      return this.#handleResponse(response);
+    } catch (e) {}
+  }
+
+  get(endpoint, params = {}, headers = {}, contentType = "application/json") {
+    const queryString = this.serializeParams(params);
+    const urlWithParams = queryString ? `${endpoint}?${queryString}` : endpoint;
+    return this.request(urlWithParams, "GET", null, headers, contentType);
+  }
+
+  post(endpoint, body, headers = {}, contentType = "application/json") {
+    return this.request(endpoint, "POST", body, headers, contentType);
+  }
+
+  put(endpoint, body, headers = {}, contentType = "application/json") {
+    return this.request(endpoint, "PUT", body, headers, contentType);
+  }
+
+  delete(endpoint, headers = {}, contentType = "application/json") {
+    return this.request(endpoint, "DELETE", null, headers, contentType);
+  }
+}
