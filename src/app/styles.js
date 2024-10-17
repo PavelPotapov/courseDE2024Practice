ba45ce840c4b47aa754e4c86e5f582441cf2f1e5
PavelPotapov <pavelpotapovv96@gmail.com>
2024-10-17 19:00:05 +0500

    feat: order styles priority

--- src/app/styles.js
+++ src/app/styles.js
@@ -6,7 +6,10 @@ const files = await import.meta.webpackContext("../", {
 // Определите массив с приоритетами
 const priorityOrder = [
   "./app/styles/vars.pcss", // Полный путь к файлу
-  "./app/styles/global.pcss", // Полный путь к файлу
+  "./app/styles/palette.pcss",
+  "./app/styles/fonts.pcss",
+  "./app/styles/global.pcss",
+  "./app/styles/utils.pcss",
   // добавьте другие файлы по мере необходимости
 ];
 
