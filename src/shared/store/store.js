c2dbaaa3882cf5deb6abbb5064f7b8e050991ea2
PavelPotapov <pavelpotapovv96@gmail.com>
2024-11-05 18:23:26 +0500

    feat: add store

---
+++ src/shared/store/store.js
@@ -0,0 +1,43 @@
+import { create } from "zustand";
+import { persist, subscribeWithSelector } from "zustand/middleware";
+
+/**
+ * Функция для создания Store с уникальным именем
+ * @param {string} storageName - Имя хранилища
+ * @return {Function} - Функция, возвращающая Store
+ */
+export const createStore = (storageName) => {
+  return create(
+    subscribeWithSelector(
+      persist(
+        (set) => ({
+          markers: [],
+          activeFilters: {},
+          setMarkers: (markers) => set({ markers }),
+          addMarker: (marker) => {
+            set((state) => {
+              // Проверка, есть ли уже маркер с таким ID
+              const exists = state.markers.some((m) => m.id === marker.id);
+              if (exists) {
+                console.warn(`Marker with ID ${marker.id} already exists.`);
+                return state; // Не изменяем состояние, если маркер с таким ID уже существует
+              }
+              return {
+                markers: [...state.markers, marker], // Добавляем новый маркер
+              };
+            });
+          },
+          removeMarker: (markerId) =>
+            set((state) => ({
+              markers: state.markers.filter((marker) => marker.id !== markerId),
+            })),
+          setFilters: (filters) => set({ activeFilters: filters }),
+        }),
+        {
+          name: storageName, // Используем переданное имя хранилища
+          getStorage: () => localStorage,
+        }
+      )
+    )
+  );
+};
