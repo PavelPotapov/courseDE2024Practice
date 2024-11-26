72745fc6847949f787eba195688f8ea8cb19059a
PavelPotapov <pavelpotapovv96@gmail.com>
2024-11-26 19:01:54 +0500

    feat: handle centering map by address on filter change

--- src/widgets/MapApp/model/index.js
+++ src/widgets/MapApp/model/index.js
@@ -46,6 +46,9 @@ export class MapApp {
   handleFilterChanged(changeData) {
     //TODO: есть замечение, касательно того, что мы всегда подвязываемся к полю inputs, а если у нас будет несколько фильтров? Нужно будет подумать над этим.
     //Тут же необходимо делать проверку если менялось поле ввода адреса и центрировать карту
+    if (changeData.search) {
+      this.handleCenterMapByAddress(changeData.search.value);
+    }
     const currentState = this.storeService.getFilters().inputs;
     const updatedState = { ...currentState, ...changeData };
     this.storeService.updateStore("setFilters", { inputs: updatedState });
